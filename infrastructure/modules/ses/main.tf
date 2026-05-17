12fae97ac93dada94c563a61c9593c80e24a0188
Mazdaratti <vasya.mazdaratti@googlemail.com>
2026-05-17 21:55:24 +0200

    fix(ses): use separate text and HTML template data

--- infrastructure/modules/ses/main.tf
+++ infrastructure/modules/ses/main.tf
@@ -20,17 +20,17 @@ resource "aws_ses_email_identity" "sender" {
 # template data instead of rendering full email bodies itself.
 resource "aws_ses_template" "event_updated" {
   name    = "${var.name_prefix}-event-updated"
-  subject = "Event updated: {{event_title}}"
+  subject = "Event updated: {{event_title_text}}"
 
   text = <<-EOT
     Hi,
 
     An event you RSVP'd to has been updated.
 
-    Event: {{event_title}}
+    Event: {{event_title_text}}
 
     Changed details:
-    {{#each changed_fields}}
+    {{#each changed_fields_text}}
     - {{this}}
     {{/each}}
 
@@ -46,12 +46,12 @@ resource "aws_ses_template" "event_updated" {
     <!doctype html>
     <html lang="en">
       <body>
-        <h1>Event updated: {{event_title}}</h1>
+        <h1>Event updated: {{event_title_html}}</h1>
         <p>An event you RSVP'd to has been updated.</p>
-        <p><strong>Event:</strong> {{event_title}}</p>
+        <p><strong>Event:</strong> {{event_title_html}}</p>
         <p><strong>Changed details:</strong></p>
         <ul>
-          {{#each changed_fields}}
+          {{#each changed_fields_html}}
           <li>{{this}}</li>
           {{/each}}
         </ul>
@@ -65,14 +65,14 @@ resource "aws_ses_template" "event_updated" {
 
 resource "aws_ses_template" "event_cancelled" {
   name    = "${var.name_prefix}-event-cancelled"
-  subject = "Event cancelled: {{event_title}}"
+  subject = "Event cancelled: {{event_title_text}}"
 
   text = <<-EOT
     Hi,
 
     An event you RSVP'd to has been cancelled.
 
-    Event: {{event_title}}
+    Event: {{event_title_text}}
 
     View the event:
     {{event_url}}
@@ -86,9 +86,9 @@ resource "aws_ses_template" "event_cancelled" {
     <!doctype html>
     <html lang="en">
       <body>
-        <h1>Event cancelled: {{event_title}}</h1>
+        <h1>Event cancelled: {{event_title_html}}</h1>
         <p>An event you RSVP'd to has been cancelled.</p>
-        <p><strong>Event:</strong> {{event_title}}</p>
+        <p><strong>Event:</strong> {{event_title_html}}</p>
         <p><a href="{{event_url}}">View the event</a></p>
         <p>You are receiving this email because you RSVP'd to this event.</p>
         <p>This is an automated notification. Please do not reply to this email.</p>

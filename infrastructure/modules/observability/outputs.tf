929c85faaeb0aba0d10a3c1b947d8b1de81c7bfe
Mazdaratti <vasya.mazdaratti@googlemail.com>
2026-05-19 15:34:48 +0200

    feat(observability): expose dashboard outputs

--- infrastructure/modules/observability/outputs.tf
+++ infrastructure/modules/observability/outputs.tf
@@ -17,3 +17,17 @@ output "alarm_arns" {
     alarm_key => alarm.arn
   }
 }
+
+############################################
+# CloudWatch dashboard outputs
+############################################
+
+output "dashboard_name" {
+  description = "Name of the CloudWatch dashboard created by the module, or null when dashboard creation is disabled."
+  value       = var.dashboard_enabled ? aws_cloudwatch_dashboard.this[0].dashboard_name : null
+}
+
+output "dashboard_arn" {
+  description = "ARN of the CloudWatch dashboard created by the module, or null when dashboard creation is disabled."
+  value       = var.dashboard_enabled ? aws_cloudwatch_dashboard.this[0].dashboard_arn : null
+}
